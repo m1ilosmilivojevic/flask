62eadb69d14f1cd5e2e8a8a07bfa594aa5e07d9a
m1ilosmilivojevic <milosmilivojevic79@gmail.com>
2025-04-15 11:47:52 +0200

    stress route

--- app.py
+++ app.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 def index():
     return "Flask CI/CD working from ECS!"
 
+@app.route('/stress')
+def stress():
+    while True: pass
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
