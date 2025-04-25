0da4bd54696687c9181d93d47be1f2deadd3c83a
m1ilosmilivojevic <milosmilivojevic79@gmail.com>
2025-04-25 19:37:01 +0200

    pls

--- app.py
+++ app.py
@@ -3,10 +3,12 @@ import pymysql
 import os
 from dotenv import load_dotenv
 
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+load_dotenv(dotenv_path=dotenv_path, override=True)
+
 app = Flask(__name__)
 
 # Load environment variables from .env file
-load_dotenv()
 DB_HOST = os.getenv("DB_HOST")
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
