1febe8e7faab71f58a4426909e6417cb50ca0734
m1ilosmilivojevic <milosmilivojevic79@gmail.com>
2025-04-25 18:46:31 +0200

    dot

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 
 app = Flask(__name__)
 
-load_dotenv()
+load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), '.env'))
 
 DB_HOST = os.getenv("DB_HOST")
 DB_USER = os.getenv("DB_USER")
